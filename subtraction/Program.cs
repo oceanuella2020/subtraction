e80211295f62db0b40ed1cff58ae4adb33c48805
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-19 22:01:40 +0100

    Add project files.

---
+++ subtraction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber;
+            int secondnumber;
+            int result;
+
+            Console.Write("Enter your first number: ");
+            firstnumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            secondnumber = Int32.Parse(Console.ReadLine());
+
+
+            result = firstnumber - secondnumber;
+            Console.WriteLine("Your result is " + result);
+
+        }
+    }
+}
